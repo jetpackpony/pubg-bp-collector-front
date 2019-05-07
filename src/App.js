2afa7db9cc182dbe1bbefa885c7a533346589f55
jetpackpony <jetpackpony@gmail.com>
2019-05-07 17:08:24 +0300

    Move the collect form into a separate component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,9 @@
-import React, { useState } from 'react';
-import moment from 'moment';
+import React from 'react';
+import CollectForm from './CollectForm';
 
 function App({ firebase }) {
-  const [rankBP, setRankBP] = useState("");
-  const [killBP, setKillBP] = useState("");
-  const [hitBP, setHitBP] = useState("");
-
-  const onSubmit = (e) => {
-    e.preventDefault()
-    firebase.sendBPData({
-      rankBP: parseInt(rankBP, 10),
-      killBP: parseInt(killBP, 10),
-      hitBP: parseInt(hitBP, 10),
-      attachedToMatch: false
-    }).then(function (docRef) {
-        console.log("Document written with ID: ", docRef.id);
-        setRankBP("");
-        setKillBP("");
-        setHitBP("");
-      })
-      .catch(function (error) {
-        console.error("Error adding document: ", error);
-      });
-  };
-
   return (
-    <div>
-      <form>
-        <div>
-          <label>
-            Rank BP:
-            <input name="rankBP" type="number" value={rankBP} onChange={e => setRankBP(e.target.value)} />
-          </label>
-        </div>
-
-        <div>
-          <label>
-            Kill BP:
-            <input id="killBP" name="killBP" type="number" value={killBP} onChange={e => setKillBP(e.target.value)} />
-          </label>
-        </div>
-
-        <div>
-          <label>
-            Hit BP:
-            <input id="hitBP" name="hitBP" type="number" value={hitBP} onChange={e => setHitBP(e.target.value)} />
-          </label>
-        </div>
-
-        <div>
-          <button onClick={onSubmit}>Submit</button>
-        </div>
-      </form>
-    </div>
+    <CollectForm firebase={firebase}></CollectForm>
   );
 }
 
