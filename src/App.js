011295b5c9183767681eb5fa95aff39427a75cd3
jetpackpony <jetpackpony@gmail.com>
2019-05-07 17:21:05 +0300

    Add simple routing to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import CollectForm from './CollectForm';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Graphs() {
+  return (
+    <h2>Graphs</h2>
+  );
+}
 
 function App({ firebase }) {
   return (
-    <CollectForm firebase={firebase}></CollectForm>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Graphs</Link>
+            </li>
+            <li>
+              <Link to="/collect">Collection Form</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Route path="/" exact component={Graphs} />
+      <Route
+        path="/collect"
+        render={(routeProps) => (
+          <CollectForm {...routeProps} firebase={firebase} />
+        )}
+      />
+    </Router>
   );
 }
 
